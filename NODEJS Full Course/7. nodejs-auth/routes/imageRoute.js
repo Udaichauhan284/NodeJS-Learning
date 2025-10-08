d3f15b0be1e97f6076314eb2feb9866c7a6de48b
Udai Chauhan <udaichauhan284@gmail.com>
2025-10-09 02:12:32 +0530

    add the delete Image functionality and added pagination

--- NODEJS Full Course/7. nodejs-auth/routes/imageRoute.js
+++ NODEJS Full Course/7. nodejs-auth/routes/imageRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const authMiddleware = require("../middlewares/authMiddleware");
 const adminMiddleware = require("../middlewares/adminMiddleware");
 const uploadMiddleware = require("../middlewares/uploadMiddleware"); 
-const {uploadImageController, fetchImageController } = require("../controllers/imageController");
+const {uploadImageController, fetchImageController, deleteImageController } = require("../controllers/imageController");
 
 
 const router = express.Router();
@@ -11,5 +11,8 @@ router.post("/upload", authMiddleware, adminMiddleware, uploadMiddleware.single(
 
 //now route for use of getting all images
 router.get("/get", authMiddleware, fetchImageController);
+
+//delete image route
+router.delete("/:id", authMiddleware, adminMiddleware, deleteImageController);
 
 module.exports = router;
