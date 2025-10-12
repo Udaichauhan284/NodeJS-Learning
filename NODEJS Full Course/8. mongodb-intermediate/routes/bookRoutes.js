d78c9493ab3b8c33352d38cf6948b21db06133a7
Udai Chauhan <udaichauhan284@gmail.com>
2025-10-12 23:07:35 +0530

    added the doing of mongodb intermediate

---
+++ NODEJS Full Course/8. mongodb-intermediate/routes/bookRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const {createAuthor, createBook, getBookWithAuthor} = require("../controllers/bookController");
+
+const router = express.Router();
+
+router.post("/author", createAuthor);
+router.post("/book" , createBook);
+router.get("/book/:id", getBookWithAuthor);
+
+module.exports = router;
--- NODEJS Full Course/8. mongodb-intermediate/server.js
+++ NODEJS Full Course/8. mongodb-intermediate/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const productRoutes = require("./routes/productRoutes");
+const bookRoutes = require("./routes/bookRoutes");
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB is connected Successfully"))
@@ -14,6 +15,7 @@ app.use(express.json());
 
 //now use the routes
 app.use("/products", productRoutes);
+app.use("/ref", bookRoutes);
 
 //now connect the app to server
 app.listen(process.env.PORT, () => {
