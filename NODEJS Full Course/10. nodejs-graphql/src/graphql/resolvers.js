9dd775054ee17acff8f7a2a06c4cc7a67e45ef3f
Udai Chauhan <udaichauhan284@gmail.com>
2025-10-20 22:52:26 +0530

    add lc graphql mutation learning and part

--- NODEJS Full Course/10. nodejs-graphql/src/graphql/resolvers.js
+++ NODEJS Full Course/10. nodejs-graphql/src/graphql/resolvers.js
@@ -17,6 +17,44 @@ const resolvers = {
         products : () => products,
         product: (_, {id}) => products.find((item) => item.id === id),
     },
-}
+
+    Mutation: {
+        createProduct : (_, {title, category, price, inStock}) => {
+            const newlyCreateProduct = {
+                id : String(products.length + 1),
+                title,
+                category,
+                price,
+                inStock
+            };
+
+            products.push(newlyCreateProduct);
+            return newlyCreateProduct;
+        },
+
+        deleteProduct : (_, {id}) => {
+            const index = products.findIndex((product) => product.id === id);
+            if(index === -1) return false;
+
+            products.splice(index, 1);
+
+            return true;
+        },
+
+        updateProduct : (_, {id, ...updates}) => {
+            const index = products.findIndex((product) => product.id === id);
+            if(index === -1) return null;
+
+            const updateProduct = {
+                ...products[index],
+                ...updates,
+            };
+
+            products[index] = updateProduct;
+
+            return updateProduct;
+        },
+    },
+};
 
 module.exports = resolvers;
