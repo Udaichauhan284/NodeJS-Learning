99986c9e55c5c586d9ea0d832bc9e580de224dee
Udai Chauhan <udaichauhan284@gmail.com>
2025-11-24 12:25:35 +0530

    add the learning of express

--- NODEJS Full-Course Part-2/express-concepts/server.js
+++ NODEJS Full-Course Part-2/express-concepts/server.js
@@ -7,6 +7,7 @@ const { requestLogger, addTimeStamp } = require("./middleware/customMiddleware")
 const { globalErrorHandler } = require("./middleware/errorHandler");
 const {urlVersioning} = require('./middleware/apiVersionong');
 const { createBasicRateLimiter } = require("./middleware/rateLimiting");
+const itemRoutes = require("./routes/itemRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,11 +16,13 @@ app.use(requestLogger);
 app.use(addTimeStamp);
 
 app.use(configureCors());
-app.use(createBasicRateLimiter(100, 15*60*1000)); //100 request per 15 minutes
+app.use(createBasicRateLimiter(2, 15*60*1000)); //100 request per 15 minutes, making it to to check
 app.use(express.json()); //express json middleware
-app.use('/api/v1', urlVersioning("v1"));
+app.use(urlVersioning("v1"));
+app.use("/api/v1", itemRoutes);
 
 app.use(globalErrorHandler);
+
 app.listen(PORT, () => {
     console.log(`Server is running and listening on ${PORT}`);
 });
